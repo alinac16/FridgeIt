4e2ba1f890800ffe008d8e46e25585c4b9a44bfc
Alina Chalanuchpong <alina.c@alumni.ubc.ca>
2021-01-02 18:05:31 -0500

    storage search bar

--- frontend/src/components/StoragePage.jsx
+++ frontend/src/components/StoragePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Page } from "react-onsenui";
 import StorageItems from "./StorageItems";
+import StorageSearch from "./StorageSearch";
 
 function StoragePage({ title, active, tabbar }) {
   return (
     <Page>
       <div>{title}</div>
+      <StorageSearch></StorageSearch>
       <StorageItems></StorageItems>
     </Page>
   );
--- frontend/src/components/StorageSearch.jsx
+++ frontend/src/components/StorageSearch.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { SearchInput } from "react-onsenui";
 
-<SearchInput
-  value={this.state.text}
-  onChange={event => {
-    this.setState({ text: event.target.value });
-  }}
-  modifier="material"
-  placeholder="Username"
-/>;
+function StorageSearch() {
+  return <SearchInput placeholder="Item Name" />;
+}
+
+export default StorageSearch;
